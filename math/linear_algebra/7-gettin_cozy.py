24930e9a9647a525c744a49d821155d449baa3d3
Lalisa <l.iriza@alustudent.com>
2026-02-03 00:36:51 +0200

    7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module to concatenate 2D matrices"""
 
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates two 2D matrices along a specific axis"""
     if axis == 0:
@@ -19,3 +20,4 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return [mat1[i][:] + mat2[i][:] for i in range(len(mat1))]
     else:
         return None
+    
